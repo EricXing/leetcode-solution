42929bf2f79967e7ceb3a44fb46b67713a369e08
Eric <ericyanzhe@gmail.com>
2018-07-13 18:24:20 +0800

    Reverse Integer

---
+++ problems/7_ReverseInteger_test.go
@@ -0,0 +1,15 @@
+package problems
+
+import "testing"
+
+func TestReverse(t *testing.T) {
+	if Reverse(123) != 321 {
+		t.Error("Reverse Integer: case 1 failed")
+	}
+	if Reverse(-123) != -321 {
+		t.Error("Reverse Integer: case 2 failed")
+	}
+	if Reverse(120) != 21 {
+		t.Error("Reverse Integer: case 3 failed")
+	}
+}
