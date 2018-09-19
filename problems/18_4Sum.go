cd36eb51df5b9d1cbb26c5bcdb1483e557f35d2f
Eric <ericyanzhe@gmail.com>
2018-09-19 14:49:52 +0800

    18. 4Sum

--- problems/18_4Sum.go
+++ problems/18_4Sum.go
@@ -23,42 +23,23 @@ func FourSum(nums []int, target int) [][]int {
 	if len(nums) < 4 {
 		return [][]int{}
 	}
-	sumSlice := make([]int, 0)
-	sliceMap := NewSliceSumMap()
-	sortedSlice := sort.IntSlice(nums)
-	sort.Stable(sortedSlice)
-	for i := 0; i < len(sortedSlice); i++ {
-		for j := i + 1; j < len(sortedSlice); j++ {
-			sum := sortedSlice[i] + sortedSlice[j]
-			sumSlice = append(sumSlice, sum)
-			sliceMap.Put(sum, []int{sortedSlice[i], sortedSlice[j]})
-		}
+	tempSlice := make([]int, len(nums))
+	for idx, num := range nums {
+		tempSlice[idx] = target - num
 	}
-
 	result := NewIntSliceSetWith4Ele()
-	for m := 0; m < len(sumSlice); m++ {
-		for n := m + 1; n < len(sumSlice); n++ {
-			sum := sumSlice[m] + sumSlice[n]
-			if sum == target {
-				combineIntSlice([][][]int{sliceMap.getSlices(sumSlice[m]), sliceMap.getSlices(sumSlice[n])}, make([][]int, 2), 0, result)
-			}
+	for i := 0; i < len(tempSlice); i++ {
+		subSlice := make([]int, 0, len(nums)-1)
+		subSlice = append(subSlice, nums[:i]...)
+		subSlice = append(subSlice, nums[i+1:]...)
+		s := threeSum(subSlice, tempSlice[i])
+		for _, val := range s {
+			resultSlice := append(val, nums[i])
+			sort.Ints(resultSlice)
+			result.Add([4]int{resultSlice[0], resultSlice[1], resultSlice[2], resultSlice[3]})
 		}
 	}
 	return result.GetValues()
-}
-
-// Cartesian Product of int slices.
-func combineIntSlice(input [][][]int, current [][]int, k int, result *IntSliceSetWith4Ele) {
-	if k == len(input) {
-		tempSlice := sort.IntSlice{current[0][0], current[0][1], current[1][0], current[1][1]}
-		sort.Stable(tempSlice)
-		result.Add([4]int{tempSlice[0], tempSlice[1], tempSlice[2], tempSlice[3]})
-	} else {
-		for j := 0; j < len(input[k]); j++ {
-			current[k] = input[k][j]
-			combineIntSlice(input, current, k+1, result)
-		}
-	}
 }
 
 type IntSliceSetWith4Ele struct {
@@ -86,35 +67,33 @@ func (set *IntSliceSetWith4Ele) GetValues() [][]int {
 	return result
 }
 
-// SliceSumMap store sum of two numbers as key, and slices of the two numbers as value.
-// There could be more than one combination.
-type SliceSumMap struct {
-	value  map[int]bool
-	slices map[int][][]int
-}
-
-func NewSliceSumMap() *SliceSumMap {
-	return &SliceSumMap{value: make(map[int]bool), slices: make(map[int][][]int, 0)}
-}
-
-func (sliceMap *SliceSumMap) Put(value int, nums []int) {
-	sliceMap.value[value] = true
-	if _, ok := sliceMap.slices[value]; ok {
-		sliceMap.slices[value] = append(sliceMap.slices[value], nums)
-	} else {
-		sliceMap.slices[value] = [][]int{nums}
-	}
-}
-
-func (sliceMap *SliceSumMap) Exist(value int) bool {
-	_, ok := sliceMap.value[value]
-	return ok
-}
-
-func (sliceMap *SliceSumMap) getSlices(value int) [][]int {
-	if _, ok := sliceMap.value[value]; ok {
-		return sliceMap.slices[value]
-	} else {
+func threeSum(nums []int, target int) [][]int {
+	if len(nums) < 3 {
 		return [][]int{}
 	}
+	var dict = NewSliceDict()
+	for idx, value := range nums {
+		dict.Put(value, idx)
+	}
+	result := NewIntSliceSet()
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			t := target - (nums[i] + nums[j])
+			if ok := dict.GetValue(t); ok {
+				indexCount := dict.GetIdx(t)
+				if nums[i] == t {
+					indexCount -= 1
+				}
+				if nums[j] == t {
+					indexCount -= 1
+				}
+				if indexCount > 0 {
+					r := sort.IntSlice{nums[i], nums[j], t}
+					sort.Stable(r)
+					result.Add([3]int{r[0], r[1], r[2]})
+				}
+			}
+		}
+	}
+	return result.GetValues()
 }
